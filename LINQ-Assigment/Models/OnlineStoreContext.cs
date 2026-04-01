d09fd88da94590f4d92ab52f15c05b424394411a
VISORKILLEN <viktor.gustavsson@live.se>
2026-04-01 09:48:38 +0200

    Made the Dbcontext file

---
+++ LINQ-Assigment/Models/OnlineStoreContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace LINQ_Assigment.Models
+{
+    internal class OnlineStoreContext : DbContext
+    {
+        // Load configuration from appsettings.json
+        private static readonly IConfiguration _config = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+        // Define DbSet properties for each entity
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
